bf7bf07de66c55b9c5ef2b24c905ba14af62325f
Oskar Lundgren <oskar.lundgren92@gmail.com>
2015-03-02 15:01:55 +0100

    Added .h-files

---
+++ main.h
@@ -0,0 +1,19 @@
+//
+//  main.h
+//  mOS
+//
+//  Created by Markus Axelsson on 2015-02-09.
+//  Copyright (c) 2015 Markus & Oskar. All rights reserved.
+//
+
+#ifndef mOS_main_h
+#define mOS_main_h
+#include "kernel.h"
+
+
+extern uint TC;
+extern int g_running_mode;
+extern int g_firstrun;
+extern TCB *Running;
+
+#endif
